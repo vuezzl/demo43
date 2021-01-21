b355b976f967a20141f0facd045b35a5a4ab874c
zzl <2585583970@qq.com>
2021-01-21 09:24:42 +0800

    路由找函数

--- zdemo/zdemo/urls.py
+++ zdemo/zdemo/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ausers.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',index)
+
 ]
